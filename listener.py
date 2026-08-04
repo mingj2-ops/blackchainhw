b5b9aa3cb547f2ff8f36cabcfb78e40f5fcb3b02
Jingjing Ming <mingj2@seas.upenn.edu>
2026-08-04 00:40:01 +0000

    fix listener

--- listener.py
+++ listener.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 from web3.providers.rpc import HTTPProvider
-from web3.middleware import ExtraDataToPOAMiddleware #Necessary for POA chains
+from web3.middleware import ExtraDataToPOAMiddleware
 from pathlib import Path
 import json
 from datetime import datetime
@@ -14,18 +14,17 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
     end_block - integer last block to scan
     contract_address - the address of the deployed contract
 
-	This function reads "Deposit" events from the specified contract, 
-	and writes information about the events to the file "deposit_logs.csv"
+    This function reads "Deposit" events from the specified contract,
+    and writes information about the events to the file "deposit_logs.csv"
     """
     if chain == 'avax':
-        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
+        api_url = f"https://api.avax-test.network/ext/bc/C/rpc"
 
     if chain == 'bsc':
-        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
+        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/"
 
     if chain in ['avax','bsc']:
         w3 = Web3(Web3.HTTPProvider(api_url))
-        # inject the poa compatibility middleware to the innermost layer
         w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
     else:
         w3 = Web3(Web3.HTTPProvider(api_url))
@@ -41,20 +40,16 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         end_block = w3.eth.get_block_number()
 
     if end_block < start_block:
-        print( f"Error end_block < start_block!" )
-        print( f"end_block = {end_block}" )
-        print( f"start_block = {start_block}" )
+        print(f"Error end_block < start_block!")
+        print(f"end_block = {end_block}")
+        print(f"start_block = {start_block}")
 
     if start_block == end_block:
-        print( f"Scanning block {start_block} on {chain}" )
+        print(f"Scanning block {start_block} on {chain}")
     else:
-        print( f"Scanning blocks {start_block} - {end_block} on {chain}" )
+        print(f"Scanning blocks {start_block} - {end_block} on {chain}")
 
-    if end_block - start_block < 30:
-        event_filter = contract.events.Deposit.create_filter(from_block=start_block,to_block=end_block,argument_filters=arg_filter)
-        events = event_filter.get_all_entries()
-        #print( f"Got {len(events)} entries for block {block_num}" )
-        # TODO YOUR CODE HERE
+    def process_events(events):
         for evt in events:
             block = w3.eth.get_block(evt['blockNumber'])
             timestamp = datetime.utcfromtimestamp(block['timestamp']).strftime('%m/%d/%Y %H:%M:%S')
@@ -71,25 +66,13 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
                 pd.DataFrame([row]).to_csv(eventfile, mode='a', header=False, index=False)
             else:
                 pd.DataFrame([row]).to_csv(eventfile, index=False)
+
+    if end_block - start_block < 30:
+        event_filter = contract.events.Deposit.create_filter(from_block=start_block, to_block=end_block, argument_filters=arg_filter)
+        events = event_filter.get_all_entries()
+        process_events(events)
     else:
-        for block_num in range(start_block,end_block+1):
-            event_filter = contract.events.Deposit.create_filter(from_block=block_num,to_block=block_num,argument_filters=arg_filter)
+        for block_num in range(start_block, end_block+1):
+            event_filter = contract.events.Deposit.create_filter(from_block=block_num, to_block=block_num, argument_filters=arg_filter)
             events = event_filter.get_all_entries()
-            #print( f"Got {len(events)} entries for block {block_num}" )
-            # TODO YOUR CODE HERE
-            for evt in events:
-            block = w3.eth.get_block(evt['blockNumber'])
-            timestamp = datetime.utcfromtimestamp(block['timestamp']).strftime('%m/%d/%Y %H:%M:%S')
-            row = {
-                'chain': chain,
-                'token': evt.args['token'],
-                'recipient': evt.args['recipient'],
-                'amount': evt.args['amount'],
-                'transactionHash': evt.transactionHash.hex(),
-                'address': evt.address,
-                'date': timestamp
-            }
-            if Path(eventfile).exists():
-                pd.DataFrame([row]).to_csv(eventfile, mode='a', header=False, index=False)
-            else:
-                pd.DataFrame([row]).to_csv(eventfile, index=False)
+            process_events(events)
